9db2c928f6c6e6e17f5456648e22f67a79d57690
rhyneav <rhyneav@gmail.com>
2016-12-29 08:55:33 -0500

    Add finish early functionality

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var round = 1;
 var dataObj = {};
 
 var done = false;
+var started = false;
 
 $.ajax({
     url:      'values.json',
@@ -19,31 +20,65 @@ $.ajax({
         $('#btn_start').prop('disabled', false);
 
         $('#lbl_valuesLeft').text(dataObj.length - index)
-
-        
     }
 });
+
+var finishEarly = function() {
+    $('#btn_very').prop('disabled', true);
+    $('#btn_middle').prop('disabled', true);
+    $('#btn_not').prop('disabled', true);
+
+    done = true;
+    $('#btn_start').text('Again!')
+
+    $('#value').hide();
+    $('#description').hide();
+
+    for (i in very) {
+        
+        $('#many_values').append('<h5 class="early_value">Value ' + (+i + 1) + '</h5>');
+        $('#many_values').append('<h2>' + very[i].value + '</h2>');
+        $('#many_values').append('<h3>' + very[i].description + '</h3>');
+        console.log(very[i].value);
+        console.log(very[i].description);
+    }
+
+        $('#lbl_top').text('Your top values are...');
+        $('#many_values').show(300);
+        $('#lbl_top').show(300);
+        $('.labels').addClass('border');
+        $('.share').show(300);
+
+};
 
 $('#btn_start').click(function() {
     if (done) {
         location.reload();
-    }
-
-    $('.labels').animate({
-        opacity: 0
-    }, 300, function() {
-        $('#btn_very').prop('disabled', false);
-        $('#btn_middle').prop('disabled', false);
-        $('#btn_not').prop('disabled', false);
-
-        updateScreen();
-
-        $('#btn_start').prop('disabled', true);
-
+    } else if (started) {
+        finishEarly();
+    } else {
         $('.labels').animate({
-            opacity: 1
-        }, 300);
-    });
+            opacity: 0
+        }, 300, function() {
+            // disable buttons
+            $('#btn_very').prop('disabled', false);
+            $('#btn_middle').prop('disabled', false);
+            $('#btn_not').prop('disabled', false);
+
+            $('.buttons').addClass('colored');
+
+            updateScreen();
+
+            // $('#btn_start').prop('disabled', true);
+            $('#btn_start').text('Finish');
+
+            $('.labels').animate({
+                opacity: 1
+            }, 300);
+
+            started = true;
+        });
+    }
 });
 
 $('#btn_very').click(function() {
@@ -77,6 +112,7 @@ var increment = function(data) {
 var shouldReset = function() {
     if (index == dataObj.length) {
         if (very.length == 0) {
+            // disable buttons
             $('#btn_very').prop('disabled', true);
             $('#btn_middle').prop('disabled', true);
             $('#btn_not').prop('disabled', true);
@@ -92,6 +128,7 @@ var shouldReset = function() {
             $('#value').text(very[0].value);
             $('#description').text(very[0].description);
 
+            // disable buttons
             $('#btn_very').prop('disabled', true);
             $('#btn_middle').prop('disabled', true);
             $('#btn_not').prop('disabled', true);
@@ -131,4 +168,5 @@ $('#btn_help').click(function() {
     if ($("#txt_help").is(':visible')) {
         $("html, body").animate({scrollTop: $("#txt_help").offset().top});
     }
-})
+});
+
